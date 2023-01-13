1dde971837e686947e358a197581f10b8f632a27
Abhishek Pandey <abhishekpandey3188@gmail.com>
2023-01-13 18:44:43 +0530

    navbar and banner added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import Banner from "./components/banner";
 import Faq from "./components/faq";
+import Navbar from "./components/navbar";
 function App() {
   return (
     <div className="App">
-      <Faq />
+      <Navbar/>
+      <Banner/>
+      {/* <Faq /> */}
     </div>
   );
 }
