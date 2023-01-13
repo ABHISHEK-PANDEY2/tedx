92410dc4e00d355ee754ecf5de8637585f8999c6
Abhishek Pandey <abhishekpandey3188@gmail.com>
2023-01-13 18:49:53 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 import Banner from "./components/banner";
 import Faq from "./components/faq";
+import Navbar from "./components/navbar";
 import Registration from "./components/registration/Registration";
 function App() {
   return (
     <div className="App">
+      <Navbar/>
+      <Banner/>
       <Registration/>
       <Faq />
     </div>
