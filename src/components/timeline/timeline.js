c53446a82ec0987f61bf359bc84829a558fa910d
Dhruv Karnwal <kdhruv.fsd@gmail.com>
2023-01-14 14:28:32 +0530

    timeline Added

---
+++ src/components/timeline/timeline.js
@@ -0,0 +1,57 @@
+import './timeline.css'
+
+import timelineElements from "./timelineElements";
+import { ReactComponent as Vectorres1 } from "../../images/Vectorres1.svg";
+import { ReactComponent as Vectorres2 } from "../../images/Vectorres2.svg";
+import { ReactComponent as Vectorres3 } from "../../images/Vectorres3.svg";
+import { ReactComponent as Vectorres5 } from "../../images/Vectorres5.svg";
+
+
+import {
+    VerticalTimeline,
+    VerticalTimelineElement,
+  } from "react-vertical-timeline-component";
+  
+  import "react-vertical-timeline-component/style.min.css";
+
+
+  
+  const Timeline = () => {
+    let workIconStyles = { background: "#202020" };
+    
+    return (
+      <div className='timeline_body'>
+      <div className='container'>
+        <h1 className="sub_title">The</h1>
+        <h1 className="title">Timeline</h1>
+      </div>
+        <VerticalTimeline>
+          {timelineElements.map((element) => {
+            let isRegister = element.icon === "register";
+            let isNotes = element.icon === "notes";
+            let isSpeaker = element.icon === "speaker";
+            return (
+              <VerticalTimelineElement
+                key={element.key}
+                date={element.date}
+                dateClassName="date"
+                iconStyle={workIconStyles}
+                // icon={element.icon}
+                icon={isRegister ? <Vectorres1 /> : isNotes ? <Vectorres2 /> : isSpeaker ? <Vectorres3 /> : <Vectorres5 />}
+              >
+                <h3 className="vertical-timeline-element-title">
+                  {element.title}
+                </h3>
+                <h5 className="vertical-timeline-element-subtitle">
+                  {element.location}
+                </h5>
+                <p id="description">{element.description}</p>
+              </VerticalTimelineElement>
+            );
+          })}
+        </VerticalTimeline>
+      </div>
+    )
+  }
+  
+  export default Timeline
---
+++ src/components/timeline/timelineElements.js
@@ -0,0 +1,85 @@
+import Vectorres1 from '../../images/Vectorres1.svg'
+let timelineElements = [
+  {
+    id: 1,
+    title: "Registeration",
+    description:
+      "Guys! register quickly limited seats available.",
+    date: "10: A.M.",
+    icon: "register",
+  },
+  {
+    id: 2,
+    title: "Welcome Notes",
+    description:
+      "Guys! register quickly limited seats available.",
+    date: "10: A.M.",
+    icon: "notes",
+  },
+  {
+    id: 3,
+    title: "Speaker",
+    description:
+      "Guys! register quickly limited seats available.",
+    date: "10: A.M.",
+    icon: "speaker",
+  },
+  {
+    id: 4,
+    title: "Speaker",
+    description:
+      "Guys! register quickly limited seats available.",
+    date: "10: A.M.",
+    icon: "speaker",
+  },
+  {
+    id: 5,
+    title: "Snacks Time",
+    description:
+      "Guys! register quickly limited seats available.",
+    date: "10: A.M.",
+    icon: "snacks",
+  },
+  {
+    id: 6,
+    title: "Registeration",
+    description:
+      "Guys! register quickly limited seats available.",
+    date: "10: A.M.",
+    icon: "speaker",
+  },
+  {
+    id: 7,
+    title: "Registeration",
+    description:
+      "Guys! register quickly limited seats available.",
+    date: "10: A.M.",
+    icon: "speaker",
+  },
+  {
+    id: 8,
+    title: "Registeration",
+    description:
+      "Guys! register quickly limited seats available.",
+    date: "10: A.M.",
+    icon: "speaker",
+  },
+  {
+    id: 9,
+    title: "Registeration",
+    description:
+      "Guys! register quickly limited seats available.",
+    date: "10: A.M.",
+    icon: "speaker",
+  },
+  {
+    id: 10,
+    title: "Registeration",
+    description:
+      "Guys! register quickly limited seats available.",
+    date: "10: A.M.",
+    icon: "snacks",
+  },
+];
+
+export default timelineElements;
